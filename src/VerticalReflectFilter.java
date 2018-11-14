f48df3accd70892cadb27f2cefa1364167628b9a
young <youngyu2002@gmail.com>
2018-11-14 11:14:17 -0800

    fix method

--- src/VerticalReflectFilter.java
+++ src/VerticalReflectFilter.java
@@ -5,7 +5,7 @@ public class VerticalReflectFilter implements PixelFilter {
         short[][] img = PixelLib.convertTo2dArray(bwpixels, width, height);
 
         int lastRowIndex = img.length - 1;
-        for (int r = 0; r < img.length; r++) {
+        for (int r = 0; r < img.length / 2; r++) {
             for (int c = 0; c < img[r].length; c++) {
                 short temp = img[r][c];
                 img[r][c] = img[lastRowIndex - r][c];

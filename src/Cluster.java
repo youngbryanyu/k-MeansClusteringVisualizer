8bb856a74bd8296353c966d9de2c93d98b3338cb
young <youngyu2002@gmail.com>
2018-12-06 18:37:30 -0800

    Deleted unnecessary methods

--- src/Cluster.java
+++ src/Cluster.java
@@ -6,10 +6,10 @@ public class Cluster {
     private Point oldCenter;
     private int r, g, b;
 
-    public Cluster() {
+    public Cluster(Point p) {
         points = new ArrayList<>();
         oldCenter = new Point(0, 0);
-        center = new Point(Math.random() * 639, Math.random() * 479);
+        center = p;
         r = (int) (Math.random() * 256);
         g = (int) (Math.random() * 256);
         b = (int) (Math.random() * 256);
@@ -47,23 +47,5 @@ public class Cluster {
 
     public String toString() {
         return "Center: (" + center.getX() + ", " + center.getY() + ")\t" + "There are " + points.size() + " points in the cluster.";
-    }
-
-    public int getRed() {
-        return r;
-    }
-
-    public int getGreen() {
-        return g;
-    }
-
-    public int getBlue() {
-        return b;
-    }
-
-    public boolean notChanging() {
-        double dx = center.getX() - oldCenter.getX();
-        double dy = center.getY() - oldCenter.getY();
-        return Math.sqrt(dx * dx + dy * dy) < 5;
     }
 }
